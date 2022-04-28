8cce6a9bb3f6b74193ac1e180ba1524e07b95d50
Sirius2 <stuart.dr@qq.com>
2022-04-28 22:20:08 +0800

    Update fast-setup.sh

--- fast-setup.sh
+++ fast-setup.sh
@@ -2,7 +2,7 @@
 
 if [ -f /usr/bin/git ];then
 	if [ ! -d  WhiteSur-desktop ];then
-		git clone https://github.com/sirius-2/ WhiteSur-desktop.git -b beautify-gtk
+		git clone https://github.com/sirius-2/WhiteSur-desktop.git -b beautify-gtk
 	fi
         if [ $? -eq 0 ];then
                 cd WhiteSur-desktop && sudo chmod +x setup.sh
